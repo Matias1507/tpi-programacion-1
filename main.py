d7a3f4449687dd3876a3e7bf070ee15130a047ef
C.V.Matias.Kurtz <126713831+Matias1507@users.noreply.github.com>
2026-06-08 19:51:41 -0300

    Update (se agregaron mas funciones) main.py

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 # ===================================================
 # TRABAJO PRÁCTICO INTEGRADOR - PROGRAMACIÓN 1
-# INTEGRANTES: Mati y Rolando
+# INTEGRANTES: Matias y Rolando
 # ===================================================
 
-# (LISTA DE PRUEBA) REALIZADO POR MATI
-# lista_paises = [
-#     {"nombre": "Argentina", "poblacion": 46000000, "superficie": 2780400, "continente": "América"},
-#     {"nombre": "Francia", "poblacion": 68000000, "superficie": 543940, "continente": "Europa"},
-#     {"nombre": "Japón", "poblacion": 125000000, "superficie": 77975, "continente": "Asia"},
-#     {"nombre": "Brasil", "poblacion": 214000000, "superficie": 8515767, "continente": "América"}
-# ]
-
-# for pais in lista_paises:
-#     print(pais["nombre"])
-
-
-# ------------------***********-----------------------
-
-
-# 2. Menú principal - (Rolando)
+# 2. Menú principal - (Rolando/Matias)
 # Usar un while True con opciones numeradas.
 # Validar que el usuario ingrese números válidos.
 
-from funciones import cargar_paises, mostrar_paises, agregar_pais, actualizar_pais, estadisticas
+from funciones import (
+    cargar_paises, 
+    mostrar_paises, 
+    agregar_pais, 
+    actualizar_pais, 
+    estadisticas, 
+    buscar_pais, 
+    ordenar_paises,
+    eliminar_pais,
+    guardar_paises
+)
 
 def menu():
     paises = cargar_paises("paises.csv")
@@ -33,7 +28,11 @@ def menu():
         print("2. Agregar país")
         print("3. Actualizar datos")
         print("4. Estadísticas")
-        print("5. Salir")
+        print("5. Buscar país")
+        print("6. Ordenar países por población")
+        print("7. Eliminar país")
+        print("8. Guardar cambios en CSV")
+        print("9. Salir")
 
         opcion = input("Elija una opción: ")
 
@@ -46,6 +45,16 @@ def menu():
         elif opcion == "4":
             estadisticas(paises)
         elif opcion == "5":
+            buscar_pais(paises)
+        elif opcion == "6":
+            ordenar_paises(paises)
+        elif opcion == "7":
+            eliminar_pais(paises)
+        elif opcion == "8":
+            guardar_paises("paises.csv", paises)
+        elif opcion == "9":
+            # Guardamos automáticamente al salir para asegurar los datos
+            guardar_paises("paises.csv", paises)
             print("Programa finalizado.")
             break
         else:
@@ -53,14 +62,3 @@ def menu():
 
 if __name__ == "__main__":
     menu()
-
-
-
-
-# Prueba de ejecución
-# paises = cargar_paises("paises.csv")
-
-# for pais in paises:
-#     print(pais["nombre"])
-
-
